c094195cb5085418947b677859f8f84b7b48fe1e
Zohaib Khan <zohaibkhan1162002@gmail.com>
2024-10-29 16:58:10 +0500

    chore: setup shadcn lib and theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
 import './globals.css';
+import { ThemeProvider } from '@/components/theme-provider';
 
 const poppins = Poppins({
   weight: ['400'],
@@ -27,9 +28,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <head></head>
-      <body className={poppins.className}>{children}</body>
+    <html lang='en' suppressHydrationWarning>
+      <body className={poppins.className}>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
