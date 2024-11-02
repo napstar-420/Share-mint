233654e02b1e7863da2c86951be5531fe61f6a6c
Khizar-sheikh <ssskhizarwaseem@gmail.com>
2024-11-03 01:33:32 +0500

    Add custom 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import SVGIMG from "./404.svg"
+import { BackgroundGrid } from "@/components/background-grid";
+import Link from "next/link";
+import { CONFIG } from "./config";
+
+export default function NotFound() {
+  return (
+    <div className="flex h-screen flex-col lg:flex-row items-center justify-center">
+        <BackgroundGrid/>
+      {/* Left Half */}
+      <div className="flex flex-col items-center justify-center text-center p-8 lg:w-1/2">
+        <h1 className="text-6xl font-bold text-[#34CB85]">404</h1>
+        <h2 className="text-2xl font-semibold mt-4 text-gray-800 dark:text-white">
+          Page Not Found
+        </h2>
+        <p className="mt-2 text-gray-600 dark:text-white">
+          This page could not be reached at this moment.
+        </p>
+        <Link
+         className="mt-6 px-6 py-2 bg-[#34CB85] text-white rounded-lg hover:bg-[#31d78a] transition duration-200"
+         href={CONFIG.ROUTE.HOME}
+         >
+          Go Back Home
+        </Link>
+      </div>
+
+      {/* Right Half */}
+      <div className="flex items-center justify-center w-full lg:w-1/2 p-8">
+        <Image
+          src={SVGIMG}
+          alt="Error Illustration"
+          width={400}
+          height={400}
+          className="max-w-full h-auto"
+        />
+      </div>
+    </div>
+  );
+}
