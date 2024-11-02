db7fbdbe06bea96d531cccbea2ec2d0d8236c88a
Zohaib Khan <zohaibkhan1162002@gmail.com>
2024-11-02 19:43:52 +0500

    chore: init and setup drizzle && neon

---
+++ app/actions.ts
@@ -0,0 +1,9 @@
+"use server";
+import 'dotenv/config'
+import { neon } from "@neondatabase/serverless";
+
+export async function getData() {
+    const sql = neon(process.env.DATABASE_URL!);
+    const data = await sql`SELECT * FROM users;`;
+    return data;
+}
