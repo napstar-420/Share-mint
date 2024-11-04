5baa8424e1ac8976b88fbc02baa96c115631b942
Zohaib Khan <zohaibkhan1162002@gmail.com>
2024-11-04 14:39:34 +0500

    fix: 404 manifest not found error

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
     ],
     apple: '/apple-touch-icon.png',
   },
-  manifest: '/site.webmanifest',
+  manifest: '/manifest.json',
 }
 
 export default function RootLayout({
