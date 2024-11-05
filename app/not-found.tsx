aa8cde3c69e9b87ca89cb5644fbdfb0e9ab2d2d2
Khizar-sheikh <ssskhizarwaseem@gmail.com>
2024-11-05 23:08:49 +0500

    Addressed review comments

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,26 +1,27 @@
 import Image from "next/image";
-import SVGIMG from "./404.svg"
+import SVGIMG from "@/404.svg";
 import { BackgroundGrid } from "@/components/background-grid";
 import Link from "next/link";
-import { CONFIG } from "./config";
+import { CONFIG } from "@/config"; // Adjusted alias
 
 export default function NotFound() {
   return (
     <div className="flex h-screen flex-col lg:flex-row items-center justify-center">
-        <BackgroundGrid/>
+      <BackgroundGrid />
+
       {/* Left Half */}
       <div className="flex flex-col items-center justify-center text-center p-8 lg:w-1/2">
-        <h1 className="text-6xl font-bold text-[#34CB85]">404</h1>
-        <h2 className="text-2xl font-semibold mt-4 text-gray-800 dark:text-white">
+        <h1 className="text-6xl font-bold text-brand-primary">404</h1>
+        <h2 className="text-2xl font-semibold mt-4 text-secondary-foreground">
           Page Not Found
         </h2>
-        <p className="mt-2 text-gray-600 dark:text-white">
+        <p className="mt-2 text-secondary-foreground">
           This page could not be reached at this moment.
         </p>
         <Link
-         className="mt-6 px-6 py-2 bg-[#34CB85] text-white rounded-lg hover:bg-[#31d78a] transition duration-200"
-         href={CONFIG.ROUTE.HOME}
-         >
+          className="mt-6 px-6 py-2 bg-brand-primary text-secondary-foreground rounded-lg hover:bg-brand-primary-hover transition duration-200"
+          href={CONFIG.ROUTE.HOME}
+        >
           Go Back Home
         </Link>
       </div>
