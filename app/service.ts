536dd985ef3cf11336691aa103befb40e30d6da0
Zohaib Khan <zohaibkhan1162002@gmail.com>
2024-11-07 20:55:26 +0500

    fix: credentials issue in vercel

--- app/service.ts
+++ app/service.ts
@@ -1,15 +1,26 @@
 import 'dotenv/config'
 import { google } from 'googleapis'
 import { GoogleAuth } from 'google-auth-library'
-import path from 'path'
 import streamifier from 'streamifier'
 import { DriveScopes } from '@/lib/enums'
 
 export const getDriveService = (scopes?: DriveScopes[]) => {
-  const KEY_FILE_PATH = path.resolve(process.cwd(), 'service.json')
+  const credentials = {
+    type: process.env.GOOGLE_TYPE,
+    project_id: process.env.GOOGLE_PROJECT_ID,
+    private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+    private_key: process.env.GOOGLE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    auth_uri: process.env.GOOGLE_AUTH_URI,
+    token_uri: process.env.GOOGLE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+    universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN,
+  }
 
   const auth = new GoogleAuth({
-    keyFile: KEY_FILE_PATH,
+    credentials,
     scopes: scopes ?? [DriveScopes.driveReadonly],
   })
   const driveService = google.drive({ version: 'v3', auth })
