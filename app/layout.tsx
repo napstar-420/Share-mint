9db6534dc46593c69b198345054676d5e9766842
Zohaib Khan <zohaibkhan1162002@gmail.com>
2024-12-18 03:08:23 +0500

    update: opengraph for homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Poppins } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from '@/components/theme-provider'
 import { Toaster } from '@/components/ui/sonner'
+import { CONFIG } from './config'
 
 const poppins = Poppins({
   weight: ['400'],
@@ -10,9 +11,12 @@ const poppins = Poppins({
   display: 'swap',
 })
 
+const title = 'Sharemint'
+const description = 'Sharemint lets you share images with end-to-end encryption and a link that automatically expires. So you can keep what you share private and make sure your stuff doesn’t stay online forever.'
+
 export const metadata: Metadata = {
-  title: 'Share mint',
-  description: 'Share images like never before',
+  title,
+  description,
   icons: {
     icon: [
       { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
@@ -20,6 +24,22 @@ export const metadata: Metadata = {
     ],
     apple: '/apple-touch-icon.png',
   },
+  openGraph: {
+    type: 'website',
+    url: CONFIG.APP_URL,
+    title,
+    description,
+    siteName: title,
+    images: [{ url: '/og-image.png' }]
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title,
+    description,
+    creator: '@napstar-420',
+    images: [{ url: '/og-image.png' }]
+  },
+  
   manifest: '/manifest.json',
 }
 
